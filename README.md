a4e28616754a6ab7517f32576a8cb74455babb3d
Winston Meikle <winmeikle@gmail.com>
2019-09-29 20:49:10 -0400

    imma happy dino

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Game-test
+#What Game Genre Are You?
+## Game-test
+
+Video games are a part of citization some genres of gaming have become staples and some people are even compared to them. This Game-test will ask you some questions and score you, depending on your score that is the type of video game genre your personality most reflect and would most likely enjoy playing. 
+
+
+Since there are so many game genres we will only be the most common genres of games. Forgive us if you felt we for got you.
+
+
+
+Enjoy the test!
---
+++ script.js
@@ -0,0 +1,27 @@
+const url = https://www.giantbomb.com/api/accessory/[guid]/?api_key=[YOURAPIKEY]  
+
+const apiKey = "a5cbc79ca2db09279291694b96f38b53f2baff94"
+
+
+
+
+// const apiKey = "9cae070c15eb864cda5e4f7c6e0a3454";
+// const requestURL = "https://api-v3.igdb.com";
+
+// axios({
+//   url: "https://api-v3.igdb.com/achievements",
+//   method: 'POST',
+//   headers: {
+//     'Accept': 'application/json',
+//     'user-key': API_KEY
+//   },
+//   data: "fields achievement_icon,category,created_at,description,external_id,game,language,locked_achievement_icon,name,owners,owners_percentage,rank,slug,tags,updated_at;"
+// })
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(err => {
+//     console.error(err);
+  // }); 
+
+
