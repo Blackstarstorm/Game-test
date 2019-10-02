004c0f92ea41137e8e3d40811d9571ba3418eaaf
Winston Meikle <winmeikle@gmail.com>
2019-10-02 15:12:46 -0400

    it works

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## What's that Pokemon Type.
 
-We currently have more that 800 Pokemon(807 officalily as of Sept 18, 2019). This app will make it so you can find Pokemon based on their types. Never agian will you forget what type a pokemon is.
+We currently have more that 800 Pokemon(807 officalily as of Sept 18, 2019). This app will make it so you can find Pokemon based on their types. Never again will you forget what type a pokemon is.
 
 First I needed to find api, after getting the api I had to see what i was able to find with it. I choose pokemon types as a way to filter out pokemon then find a out where the names where located(type.pokemon.name)
 
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const baseURL = "https://pokeapi.co/api/v2/type/"
-// const search = document.querySelector()
 const button = document.querySelector("button")
 const searchType = document.querySelector(".pokemon-list")
 const input = document.querySelector("#types")
-
+const display = document.querySelector("#display")
 
 let renderList = (types) => {
   searchType.innerHTML = "<h2>Pokemon List</h2>"
@@ -11,47 +10,38 @@ let renderList = (types) => {
     const typeOutput = document.createElement('div')
     typeOutput.innerHTML = `<h3>${type.pokemon.name}</h3><button id=${type.pokemon.name} class="more-info">Show Me</button>`;
     searchType.append(typeOutput);
-  }) //
+  })
 
   const learnButton = document.querySelectorAll(".more-info")
-  console.log(learnButton)
-  // const sprites = document.querySelectorAll(".more-info")
-  // console.log(button.id.front_default)
-  learnButton.forEach((button, index) => {
-    button.addEventListener("click", async () => {
-      const sprite = await axios.get(`${type[index].url}`)
-      console.log(sprite)
 
+  learnButton.forEach((button) => {
+    button.addEventListener("click", async () => {
+      display.innerHTML = ""
+      const name = button.id;
+      const sprite = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      let pic = sprite.data.sprites.front_default
+      const pokeDiv = document.createElement("div")
+      pokeDiv.innerHTML = `<img src=${pic} />`
+      display.append(pokeDiv)
     })
   })
 }
 
 const getData = async () => {
   const pokeType = input.value
-  try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}/`)
-    pokemonList = response.data.pokemon
-    console.log(pokemonList)
-    renderList(pokemonList)
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}/`)
+  pokemonList = response.data.pokemon
+  // console.log(pokemonList)
+  renderList(pokemonList)
 }
 
 button.addEventListener("click", getData)
 
-window.onload = function () {
-
-}
 
 
 
 
 
 
-// button.addEventListener("click", async () => {
-//   let type = search.value;
-//   let response = await axios.get(`${baseURL}${type}/`)
-//   let typeList = response.data.Search;
-//   renderList(typeList);
-// });
+
+
