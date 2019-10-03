a2fec2f67acaa981212c60fafc0165a57aecd10e
Winston Meikle <winmeikle@gmail.com>
2019-10-03 12:33:10 -0400

    done

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const baseURL = "https://pokeapi.co/api/v2/type/"
-const button = document.querySelector("button")
+const button = document.querySelector("#search")
 const searchType = document.querySelector(".pokemon-list")
 const input = document.querySelector("#types")
+const modal = document.querySelector("#modal")
 const display = document.querySelector("#display")
+const displayBack = document.querySelector("#displayBack")
+console.log("test")
 
 let renderList = (types) => {
-  searchType.innerHTML = "<h2>Pokemon List</h2>"
+  searchType.innerHTML = ""
   types.forEach((type) => {
     const typeOutput = document.createElement('div')
     typeOutput.innerHTML = `<h3>${type.pokemon.name}</h3><button id=${type.pokemon.name} class="more-info">Show Me</button>`;
@@ -16,23 +19,28 @@ let renderList = (types) => {
 
   learnButton.forEach((button) => {
     button.addEventListener("click", async () => {
+      modal.style.display = "block"
       display.innerHTML = ""
       const name = button.id;
       const sprite = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       let pic = sprite.data.sprites.front_default
       const pokeDiv = document.createElement("div")
-      pokeDiv.innerHTML = `<img src=${pic} />`
+      pokeDiv.innerHTML = pic ? `<img src=${pic} />` : `<p>This Pokemon has yet to be photographed!</p>`
       display.append(pokeDiv)
     })
   })
 }
 
+displayBack.addEventListener("click", function () {
+  modal.style.display = "none";
+})
+
 const getData = async () => {
   const pokeType = input.value
   const response = await axios.get(`https://pokeapi.co/api/v2/type/${pokeType}/`)
   pokemonList = response.data.pokemon
-  // console.log(pokemonList)
   renderList(pokemonList)
+
 }
 
 button.addEventListener("click", getData)
